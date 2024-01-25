c00bbbe31e1ee47c9f09babd801b540cc841079e
ita <ita201125@gmail.com>
2024-01-25 16:39:24 +0800

    fix(directive): 🧩 修改系统名称

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,7 +5,7 @@
       <div class="login-form">
         <div class="login-logo">
           <img class="login-icon" src="@/assets/images/logo.svg" alt="" />
-          <h2 class="logo-text">Admin</h2>
+          <h2 class="logo-text">{{ env.VITE_GLOB_APP_TITLE }}</h2>
         </div>
         <LoginForm />
       </div>
@@ -16,6 +16,7 @@
 <script setup lang="ts" name="login">
 import LoginForm from "./components/LoginForm.vue";
 import SwitchDark from "@/components/SwitchDark/index.vue";
+const env = import.meta.env;
 </script>
 
 <style scoped lang="scss">
